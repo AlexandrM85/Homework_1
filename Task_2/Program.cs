a8653d4a67ea1de08bd9b6f08ab48a5a369121af
Alex <automex74@gmail.com>
2022-10-01 16:28:13 +0500

    add task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введи первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введи второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введи третье число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max= ");
+Console.WriteLine(max);
