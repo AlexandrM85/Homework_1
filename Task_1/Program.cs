b658f17fef438e74de2532324bb6e946181db625
Alex <automex74@gmail.com>
2022-10-01 16:17:52 +0500

    add tsk_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введи первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введи второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+
+if(num1 > num2) max = num1;
+if(num1 < num2) max = num2;
+
+Console.Write("max= ");
+Console.WriteLine(max);
