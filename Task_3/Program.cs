8c782fa6af854a0109a7af31d94410f4cff32222
Alex <automex74@gmail.com>
2022-10-01 23:47:00 +0500

    add task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num%2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
