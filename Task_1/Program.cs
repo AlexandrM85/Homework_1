83b32831f1773560be2c6c4b7125c5e11bdab96b
Alex <automex74@gmail.com>
2022-10-01 23:32:32 +0500

    add task_1_option_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введи первое число: ");
+﻿//Вариант 1.
+/*Console.WriteLine("Введи первое число: ");
 int num1 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введи второе число: ");
@@ -10,4 +11,25 @@ if(num1 > num2) max = num1;
 if(num1 < num2) max = num2;
 
 Console.Write("max= ");
+Console.WriteLine(max);*/
+
+//Вариант 2.
+Console.WriteLine("Введи первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введи второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+int min = num2;
+
+if(num1 > num2) max = num1;
+if(num1 > num2) min = num2;
+if(num1 < num2) max = num2;
+if(num1 < num2) min = num1;
+
+Console.Write("max= ");
 Console.WriteLine(max);
+
+Console.Write("min= ");
+Console.WriteLine(min);
