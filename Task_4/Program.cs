6d3de2bc342857af1a20836fa8adfb902c2a85a6
Alex <automex74@gmail.com>
2022-10-02 12:58:54 +0500

    add task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+while (result <= num)
+{
+    if (result%2 == 0)
+    Console.WriteLine(result);
+    result++;
+}
